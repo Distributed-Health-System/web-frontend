f8f12c97e937970292ee0f42f0da0b30b9e5d1a9
lakindu62 <lakindu62@gmail.com>
2026-04-16 12:23:56 +0530

    fix: update import paths for utility functions in UI components

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 
-import { cn } from "../../../lib/utils"
+import { cn } from "@/lib/utils"
 
 // Base input styles with built-in invalid and focus-visible states.
 function Input({ className, type, ...props }: React.ComponentProps<"input">) {
